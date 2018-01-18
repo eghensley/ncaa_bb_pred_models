de70a72d512fc609a9f0b7a2e474f7e5982a8c13
erichensleyibm <eric.hensley@ibm.com>
2018-01-18 01:18:17 -0500

    push

--- lgb_tuning_chemistry.py
+++ lgb_tuning_chemistry.py
@@ -10,47 +10,69 @@ data = pd.read_csv('chemistry_data.csv')
 del data['Unnamed: 0']
 x_feats = list(data)
 x_feats.remove('chemistry')
-def check_lr(lr):
+def check_lr(lr, x, y):
     scores = []
-    for tree in [50, 75, 100, 150, 200]:
-        model = lgb.LGBMRegressor(random_state = 1108, n_estimators = tree, subsample = .8, learning_rate = lr)
-        score = cross_val_score(model, data[x_feats], data['chemistry'], scoring = 'explained_variance' ,cv = KFold(n_splits = 5, random_state = 46))
+    for tree in [50, 100, 150]:
+        test = lgb.LGBMRegressor(random_state = 1108, n_estimators = tree, subsample = .8, learning_rate = lr)
+        score = cross_val_score(test, x, y, scoring = 'explained_variance' ,cv = KFold(n_splits = 5, random_state = 46))
         scores.append(np.mean(score))
-    if scores[2] == max(scores):
-        return True
-    else:
-        return scores.index(max(scores))
+    return scores.index(max(scores))
 
-x = check_lr(.01)
-model = lgb.LGBMRegressor(random_state = 1108, n_estimators = 100, subsample = .8)
+def find_lr(start_lr, x, y):
+    last = None
+    while start_lr:
+        x = check_lr(start_lr, x, y)
+        if x == 0:
+            if last == 2:
+                learn_rate = np.mean([start_lr, start_lr /2])
+                print('Learning Rate: %s' % (learn_rate))
+                start_lr = False
+            else:
+                start_lr /= 2
+        elif x==1:
+            learn_rate = start_lr
+            print('Learning Rate: %s' % (learn_rate))
+            start_lr = False
+        elif x==2:
+            if last == 0:
+                learn_rate = np.mean([start_lr, start_lr/2])
+                print('Learning Rate: %s' % (learn_rate))
+                start_lr = False
+            else:
+                start_lr *= 2
+        last = x
+    return learn_rate
+
+
+learn_rate = find_lr(.01, data[x_feats], data['chemistry'])
+model = lgb.LGBMRegressor(random_state = 1108, n_estimators = 100, subsample = .8, learning_rate = learn_rate)
 model.fit(data[x_feats], data['chemistry'])
 sigs = model.feature_importances_
 indices = np.argsort(sigs)[::-1]
 feat_sigs = [x_feats[i-1] for i in indices]
 
-
-
-
 def sample_loss_n_feats(parameters):
     feats = int(parameters[0])
-    model = lgb.LGBMRegressor(random_state = 1108, n_estimators = 100, subsample = .8)
+    model = lgb.LGBMRegressor(random_state = 1108, n_estimators = 100, subsample = .8, learning_rate = learn_rate)
     score = cross_val_score(model, data[feat_sigs[:feats]], data['chemistry'], scoring = 'explained_variance' ,cv = KFold(n_splits = 5, random_state = 46))
     print(np.mean(score))
     return np.mean(score)
 
-
-
 def find_feats():
     bounds = np.array([[10, 100]])
-    start = [[100]]
-    results = bayesian_optimisation(n_iters=10,  
+    start = [[70]]
+    results = bayesian_optimisation(n_iters=20,  
                           sample_loss=sample_loss_n_feats, 
                           bounds=bounds,
                           x0 = start,
                           gp_params = {'kernel': Matern(), 'alpha': 1e-5, 'n_restarts_optimizer': 10, 'normalize_y': True})
     return int(results[0][list(results[1]).index(max(results[1]))])
-    
 feats = find_feats()
+new_learn_rate = find_lr(.01, data[feat_sigs[:feats]], data['chemistry'])
+
+
+
+
 result_list = []
 params_list = []
 right = .2
